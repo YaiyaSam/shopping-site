7f3856b1611735cee7095580b6e252512321bc43
Sanjana Navarathne <62108894+Sanjana-99@users.noreply.github.com>
2022-10-28 08:29:28 +0530

    Adding shopping cart and checkout (#1)

---
+++ src/pages/CartItem.js
@@ -0,0 +1,34 @@
+import { HiOutlineTrash } from "react-icons/hi";
+
+const CartItem = ({item}) => {
+
+    const removeItem = () => {
+    //delete request to remove a product
+    };
+
+    return(
+        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+            <div class="flex w-1/5"> 
+              <div class="flex flex-col justify-between ml-2 flex-grow">
+                <p className="font-semibold">{item.title}</p>
+              </div>
+            </div>
+            <div class="flex justify-center w-1/5">
+              <div className="w-1/5">
+                <input
+                  type="number"
+                  defaultValue={item.qty}
+                  className="text-center outline-none border-2 border py-1 w-4/5 rounded-lg"
+                />
+              </div>
+            </div>
+            <p className="text-center w-1/5 font-semibold text-sm">{item.price}</p>
+            <p className="text-center w-1/5 font-semibold text-sm">{item.price * item.qty}</p> 
+            <div class="flex justify-center w-1/5">
+              <HiOutlineTrash size="20px" onClick={removeItem}/>
+            </div>
+          </div>
+    )
+}
+
+export default CartItem;
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,54 +1,71 @@
-import { Link } from "react-router-dom";
+import CartItem from "./CartItem";
 
 function Checkout() {
+
+  const checkoutFunction = () => {
+    //checkout
+  };
+
     const items = [
       {
         id: 1,
         title: "title 1",
         desc: "dskdnksadsa dsja hdjsa d dhasj ",
         qty: 1,
-        price: "3450.00",
+        price: "3450",
       },
       {
         id: 2,
         title: "title 2",
         desc: "dskdnksadsa dshda dsd shdkjas dhsa dhjsa dahsd hsajd sahddsdsd m dsjd",
         qty: 2,
-        price: "1000.00",
+        price: "1000",
       },
       {
         id: 3,
         title: "title 3",
         desc: "dskdnksadsa dshdk s s dakj dkadas",
         qty: 1,
-        price: "1200.00",
+        price: "1200",
       }
     ];
-    
-  return (
-      <div>
-      <div className="flex w-screen px-10 items-center">
-        <div className="w-1/3 py-10 pl-10">
-          <p className="text-3xl font-semibold mb-2">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-            nonummy nibh
-          </p>
-          <p className="mb-4">
-            Lorem ipsum ut laoreet dolore magna aliquam erat volutpat. Ut wisi
-            enim ad minim veniam, quis
-          </p>
-          <Link
-            to={"/shop"}
-            className="bg-blue-600 text-white px-5 py-1.5 rounded-lg"
-          >
-            Shop
-          </Link>
+
+    return (
+      <div class="container px-20 pt-10">
+        <div class="flex shadow-md my-10">
+          <div class="w-3/4 bg-white px-10 py-10">
+            <div class="flex justify-between border-b pb-8">
+              <h1 class="font-semibold text-xl">Shopping Cart</h1>
+            </div>
+            <div class="flex mt-10 mb-5">
+              <p className="text-gray-600 text-sm uppercase w-1/5">Product</p>
+              <p className="text-center text-gray-600 text-sm uppercase w-1/5 text-center">Quantity</p>
+              <p className="text-center text-gray-600 text-sm uppercase w-1/5 text-center">Price</p>
+              <p className="text-center text-gray-600 text-sm uppercase w-1/5 text-center">Total</p>
+            </div>
+            {items.map((item) => (
+            <CartItem item={item} />
+            ))}
+          </div>
+          <div class="w-1/4 px-8 py-10 bg-slate-50">
+            <h1 class="font-semibold text-xl border-b pb-8">Order Summary</h1>
+            <div class="flex justify-between mt-10 mb-5">
+              <p className="font-semibold text-sm uppercase"># Items</p>
+              <p className="font-semibold text-sm uppercase">3</p>
+            </div>
+            <div class="flex justify-between py-6">
+              <p className="font-semibold text-sm uppercase">Total cost</p>
+              <p className="font-semibold text-sm uppercase">600</p>
+            </div>
+            <button
+              className="bg-blue-600 rounded-lg text-white font-semibold py-3 text-sm uppercase w-full"
+              onClick={checkoutFunction}
+              >
+              Checkout
+            </button>
+          </div>
         </div>
-        <div className="w-2/3">
-          <img src="/assets/5464026.jpg" alt="" />
-        </div>
-      </div>
-    </div>
+      </div>  
   );
 }
 
